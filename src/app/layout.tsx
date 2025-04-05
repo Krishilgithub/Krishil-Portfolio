358026f0841c9de93da9d1be4b00a0aba8925c55
Krishilgithub <100427701+Krishilgithub@users.noreply.github.com>
2025-04-05 09:38:26 +0530

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import AppWrapper from "@/components/AppWrapper";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AppWrapper>{children}</AppWrapper>
+        <Analytics />
       </body>
     </html>
   );
